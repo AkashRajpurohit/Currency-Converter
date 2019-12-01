b90f1dc21facd9afcaf47493a9bc54003130b871
Akash Rajpurohit <akashrajpurohit.009@gmail.com>
2019-12-01 22:44:11 +0530

    :art: show scaffold while initial data is loading

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,4 @@
-import 'package:currency_converter/screens/dashboard.dart';
+import 'package:currency_converter/services/currency.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -9,6 +9,23 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return DashboardPage(currenyOne: 'USD', currencyTwo: 'INR', currencyVal: 1.00, convertedCurrency: 74.32, isWhite: false);
+
+    CurrencyService().convertCurrency(
+      fromCurrency: 'USD',
+      toCurrency: 'INR',
+      amount: 1,
+      context: context,
+      isWhite: false
+    );
+
+    return Scaffold(
+      backgroundColor: Colors.red[700],
+      body: Center(
+        child: CircleAvatar(
+          backgroundImage: AssetImage('assets/icon.png'),
+          radius: 75.0,
+        ),
+      ),
+    );
   }
 }
